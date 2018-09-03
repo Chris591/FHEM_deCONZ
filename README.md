c48a5c6469b9baa385026786db67c8035800807f
Christoph Warnke <warnke@live.com>
2018-09-03 22:11:47 +0200

    change controls file to UTF8

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FHEM_deCONZ
 
-update add https://github.com/Chris591/FHEM_deCONZ/raw/master/controls_deCONZ.txt
+update add https://raw.githubusercontent.com/Chris591/FHEM_deCONZ/master/controls_deCONZ.txt
 
-update all https://github.com/Chris591/FHEM_deCONZ/raw/master/controls_deCONZ.txt
+update all https://raw.githubusercontent.com/Chris591/FHEM_deCONZ/master/controls_deCONZ.txt
--- create_FHEM_controls.ps1
+++ create_FHEM_controls.ps1
@@ -1,6 +1,6 @@
-Out-File .\controls_deCONZ.txt -Force -NoNewline
+Out-File .\controls_deCONZ.txt -Force -NoNewline -Encoding utf8
 foreach($file in Get-ChildItem -Path .\FHEM) {
     $date = $file.LastWriteTimeUtc.ToString("yyyy_MM_dd_HH:mm:ss")
     $size = $file.Length
-    "UPD $date $size " + $file.Directory.Name+ "/" + $file.Name + "`n" | Out-File .\controls_deCONZ.txt -Append -NoNewline
+    "UPD $date $size " + $file.Directory.Name+ "/" + $file.Name + "`n" | Out-File .\controls_deCONZ.txt -Append -NoNewline -Encoding utf8
 }
