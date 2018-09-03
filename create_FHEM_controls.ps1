28cfe9ee80402733afcf2e592f613bc1688ff7d8
Christoph Warnke <warnke@live.com>
2018-09-03 22:23:24 +0200

    change controles script

--- create_FHEM_controls.ps1
+++ create_FHEM_controls.ps1
@@ -1,6 +1,7 @@
-Out-File .\controls_deCONZ.txt -Force -NoNewline -Encoding utf8
+$Filename = ".\controls_deCONZ.txt"
+Remove-Item $Filename -Force
 foreach($file in Get-ChildItem -Path .\FHEM) {
     $date = $file.LastWriteTimeUtc.ToString("yyyy_MM_dd_HH:mm:ss")
     $size = $file.Length
-    "UPD $date $size " + $file.Directory.Name+ "/" + $file.Name + "`n" | Out-File .\controls_deCONZ.txt -Append -NoNewline -Encoding utf8
+    "UPD $date $size " + $file.Directory.Name+ "/" + $file.Name >> $Filename
 }
