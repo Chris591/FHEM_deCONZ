b3fd1353b80bda68782654826ac94dff419727d4
Christoph Warnke <warnke@live.com>
2018-09-03 21:38:59 +0200

    create an Update script for FHEM

---
+++ create_FHEM_controls.ps1
@@ -0,0 +1,6 @@
+Out-File .\controls_fhem_deCONZ.txt -Force
+foreach($file in Get-ChildItem -Path .\FHEM) {
+    $date = $file.LastWriteTimeUtc.ToString("yyyy_MM_dd_HH:mm:ss")
+    $size = $file.Length
+    "UPD $date $size " + $file.Directory.Name+ "/" + $file.Name | Out-File .\controls_fhem_deCONZ.txt -Append
+}
